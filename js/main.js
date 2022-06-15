8e2d687e94be22fe5b7e4d3a848140ddadc2b288
EvoX7 <creativesoultouch@gmail.com>
2022-06-15 17:35:17 +0200

    Commit

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let message;
 
 // Defined initial ticket cost
 let cost = distanceKm * 0.21;
-console.log("Your total Trip cost is:" + cost);
+// console.log("Ticket cost is:"+ cost.toFixed(2) + "€");
 
 // Discount conditions 
 if (userAge <= 17) {
@@ -19,12 +19,24 @@ if (userAge <= 17) {
     message = ("You are entitled to a 40% discount! Ticket cost is:" + cost.toFixed(2) + "€");
 
 } else {
-
     message = ("Ticket cost is:" + cost.toFixed(2) + "€");
 
 }
 
 console.log(message);
+
+const submitButton = document.querySelector("#generate-btn");
+const outputCost = document.querySelector("#ticketCost");
+
+
+submitButton.addEventListener("click", function(){
+    console.log(document.getElementById("inputName").value);
+    console.log(document.getElementById("inputDistance").value);
+    console.log(document.getElementById("inputAge").value);
+    outputCost.innerHTML = console.log(document.getElementById("inputName").value);
+
+  
+});
 
 
 
