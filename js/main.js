5494a300ace216209d9e8737654133818597267a
EvoX7 <creativesoultouch@gmail.com>
2022-06-15 19:29:24 +0200

    Working on screen info

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const submitButton = document.querySelector("#generate-btn");
-const outputCost = document.querySelector("#ticketCost");
+
 
 submitButton.addEventListener("click", function () {
+
   // User input
+  const userName = document.getElementById("inputName").value;
   const distanceKm = document.getElementById("inputDistance").value;
   const userAge = document.getElementById("inputAge").value;
 
@@ -11,28 +13,27 @@ submitButton.addEventListener("click", function () {
 
   // Defined initial ticket cost
   let cost = distanceKm * 0.21;
-  // console.log("Ticket cost is:"+ cost.toFixed(2) + "€");
 
   // Discount conditions
   if (userAge <= 17) {
     cost = cost - (cost * 20) / 100;
     message =
-      "You are entitled to a 20% discount! Ticket cost is:" +
+      "You are entitled to a 20% discount! Ticket cost is: " +
       cost.toFixed(2) +
       "€";
   } else if (userAge >= 65) {
     cost = cost - (cost * 40) / 100;
     message =
-      "You are entitled to a 40% discount! Ticket cost is:" +
+      "You are entitled to a 40% discount! Ticket cost is: " +
       cost.toFixed(2) +
       "€";
   } else {
-    message = "Ticket cost is:" + cost.toFixed(2) + "€";
+    message = "Ticket cost is: " + cost.toFixed(2) + "€";
   }
 
-  console.log(message);
 
   console.log(document.getElementById("inputName").value);
   console.log(document.getElementById("inputDistance").value);
   console.log(document.getElementById("inputAge").value);
+  document.getElementById("ticketCost").innerHTML = `${message}`;
 });
